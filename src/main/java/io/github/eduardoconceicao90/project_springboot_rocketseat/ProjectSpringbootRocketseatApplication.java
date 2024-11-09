56281314ad15ba5833584b2eac21d660c2c5192f
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-11-09 15:21:39 -0300

    first commit

---
+++ src/main/java/io/github/eduardoconceicao90/project_springboot_rocketseat/ProjectSpringbootRocketseatApplication.java
@@ -0,0 +1,13 @@
+package io.github.eduardoconceicao90.project_springboot_rocketseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootRocketseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootRocketseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/eduardoconceicao90/project_springboot_rocketseat/ProjectSpringbootRocketseatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eduardoconceicao90.project_springboot_rocketseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootRocketseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
