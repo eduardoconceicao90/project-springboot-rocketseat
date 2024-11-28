f5b2b3b561eab9e3b3307f610f7386b5b37550d8
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-11-27 23:20:16 -0300

    Testando exceções

---
+++ src/test/java/io/github/eduardoconceicao90/project_springboot_rocketseat/candidate/CandidateTest.java
@@ -0,0 +1,36 @@
+package io.github.eduardoconceicao90.project_springboot_rocketseat.candidate;
+
+import io.github.eduardoconceicao90.project_springboot_rocketseat.exception.UserNotFoundException;
+import io.github.eduardoconceicao90.project_springboot_rocketseat.repository.CandidateRepository;
+import io.github.eduardoconceicao90.project_springboot_rocketseat.repository.JobRepository;
+import io.github.eduardoconceicao90.project_springboot_rocketseat.service.CandidateService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CandidateTest {
+
+    @InjectMocks
+    private CandidateService candidateService;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("Should not be able to apply job with candidate not found")
+    public void shouldNotBeAbleToApplyJobWithCandidateNotFound() {
+        try {
+            candidateService.execute(null, null);
+        } catch (Exception e) {
+            Assertions.assertEquals(UserNotFoundException.class, e.getClass());
+        }
+    }
+}
